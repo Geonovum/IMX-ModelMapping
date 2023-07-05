c05517886eb0a8cb42e00a1259c402715c76c02e
Arnoud de Boer <a.deboer@geonovum.nl>
2023-07-05 16:07:30 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,17 +4,17 @@ let respecConfig = {
   useLabel: true,
   //default voor Geonovum is cc-by, eventueel is het mogelijk een andere licentie (cc0 of cc-by-nd) hier te kiezen voor het document.
   // license: "cc-by",
-  specStatus: "WV",
+  specStatus: "CV",
   specType: "ST",
-  pubDomain: "wau",
-  shortName: "WaU-MAP",
-  publishDate: "2023-04-03",
+  pubDomain: "IMX-ModelMapping",
+  shortName: "IMX-ModelMapping",
+  //publishDate: "2023-04-03",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we standaard alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   // previousPublishDate: "2014-05-01",
   previousMaturity: "EO",
-  title: "WaU IMX Model Mapping",
+  title: "IMX Model Mapping",
   editors:
     [
       {
@@ -27,12 +27,12 @@ let respecConfig = {
     [
       {
         name: "Pano Maria",
-        company: "Skemu",
-        companyURL: "https://www.skemu.com",
+        company: "Geonovum",
+        companyURL: "https://www.geonovum.nl",
       }
     ],
   //neem hier de URL van de github repository op waar het respec document in staat
-  github: "https://github.com/Geonovum/WaU-MAP",
+  github: "https://github.com/Geonovum/IMX-ModelMapping",
 
 
   // Create PDF and link to file in header (optional):
